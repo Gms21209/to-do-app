fe7c40408db831109a37dfe5a99c465ce3e20251
Max <maxschnitzer95@gmail.com>
2020-07-01 15:02:24 -0500

    checkpoint-12-12: completed

--- app.js
+++ app.js
@@ -2,14 +2,26 @@ function onReady() {
   const ADD_TODO_FORM = document.getElementById('addToDoForm');
   const NEW_TODO_TEXT = document.getElementById('newToDoText');
   const TODO_LIST = document.getElementById('toDolist');
+
   ADD_TODO_FORM.addEventListener('submit', function(event) {
     event.preventDefault();
+
     let title = NEW_TODO_TEXT.value;
+
     let newLi = document.createElement('li');
     let checkbox = document.createElement('input');
     checkbox.type = "checkbox";
+
+    let deleteBtn = document.createElement('button');
+    deleteBtn.textContent = "Delete";
+
+    deleteBtn.addEventListener('click', function(event) {
+      TODO_LIST.removeChild(this.parentElement);
+    })
+
     newLi.textContent = title;
     newLi.appendChild(checkbox);
+    newLi.appendChild(deleteBtn);
     TODO_LIST.appendChild(newLi);
     NEW_TODO_TEXT.value = '';
   });
